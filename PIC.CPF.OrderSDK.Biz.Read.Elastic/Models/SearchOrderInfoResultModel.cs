5ddea42d277d8e6573a71484faeeda97dce8caaf
YC <Moerasermax@gmail.com>
2026-04-29 16:50:34 +0800

    給客戶的版本

--- PIC.CPF.OrderSDK.Biz.Read.Elastic/Models/SearchOrderInfoResultModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PIC.CPF.OrderSDK.Biz.Read.Elastic.Models
-{
-    public class SearchOrderInfoResultModel
-    {
-        public OrderInfoModel[]? OrderInfos { get; set; }
-        public long Total { get; set; }
-        public long Took { get; set; }
-    }
-}
